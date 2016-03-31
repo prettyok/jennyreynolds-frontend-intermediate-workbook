a142c71a9150408c6ca86715a6937a8c9bd59084
prettyok <jenny@Jennys-MacBook-Pro.local>
2016-03-30 20:51:05 -0500

    Towers of Finished

--- apps/08towers-of-hanoi/script.js
+++ apps/08towers-of-hanoi/script.js
@@ -2,4 +2,37 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+    var block = null;
+    
+    $("[data-stack]").click(function() {
+        if (block === null) {
+            block = $(this).children().last().detach();
+        }
+        else {
+            $(this).append(block);
+            block = null;
+            checkForWin();
+        }
+    });
+    
+    function checkForWin() {
+        // Your code here
+        if ($('[data-stack="2"]').children().length === 4 || ($('[data-stack="3"]').children().length === 4 )) {
+            $("#announce-game-won").text("You won!");
+
+            //turn off click if winner
+            $( "[data-cell]" ).off();
+        
+        } 
+    }
+    
+});
+
+/*function checkForWin() {
+        if (checkForWin).each()) {
+            $("#announce-winner").text("Player " + playerTurn + " Wins!");
+            //turn off click if winner
+            $( "[data-cell]" ).off();
+        }
+
+    }*/
